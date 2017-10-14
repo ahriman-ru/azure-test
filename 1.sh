3e0b889a4e96dda770ab16b35507184032fa71b9
Alex Belotserkovskiy <albe@microsoft.com>
2017-10-15 00:08:33 +0300

    _

--- 1.sh
+++ 1.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 MASTER_NAME=$1
 MASTER_IP=$2
-touch /tmp/$MASTER_IP
-touch /tmp/$MASTER_NAME
-echo $MASTER_IP > /tmp/wewerehere
-echo $MASTER_IP $MASTER_NAME >> /etc/hosts
+touch /tmp/wwwww
+echo $MASTER_IP > /tmp/wewerehere > /tmp/azuredeploy123.log
+#echo $MASTER_IP $MASTER_NAME >> /etc/hosts
 date > /tmp/azuredeploy123.log
