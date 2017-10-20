3f6f7285ca3effca2a863c37ed33827bdc7ea2f0
Alex Belotserkovskiy <albe@microsoft.com>
2017-10-20 12:14:22 +0300

    _

--- 1.sh
+++ 1.sh
@@ -19,20 +19,3 @@ if ! [ -f /home/$MASTER_USER_NAME/.ssh/id_rsa ]; then
     sudo -u $MASTER_USER_NAME sh -c "ssh-keygen -f /home/$MASTER_USER_NAME/.ssh/id_rsa -t rsa -N ''" >> /tmp/sshlog.log
 fi
 
-#torque MASTER installation
-cd /tmp
-wget http://www.adaptivecomputing.com/index.php?wpfb_dl=3212 -O torque.tar.gz
-tar xzvf torque.tar.gz >> /tmp/azuredeploy.log.torque.$$ 2>&1
-cd torque-6.1.1* >> /tmp/azuredeploy.log.torque.$$ 2>&1
-
-sudo ./configure >> /tmp/azuredeploy.log.torque.$$ 2>&1
-make >> /tmp/azuredeploy.log.torque.$$ 2>&1
-make packages >> /tmp/azuredeploy.log.torque.$$ 2>&1
-sudo make install >> /tmp/azuredeploy.log.torque.$$ 2>&1
-# Create and start trqauthd
-sudo cp contrib/systemd/trqauthd.service /usr/lib/systemd/system/ >> /tmp/azuredeploy.log.torque.$$ 2>&1
-sudo systemctl enable trqauthd.service >> /tmp/azuredeploy.log.torque.$$ 2>&1
-sudo ldconfig >> /tmp/azuredeploy.log.torque.$$ 2>&1
-sudo systemctl start trqauthd.service >> /tmp/azuredeploy.log.torque.$$ 2>&1
-sudo ./torque.setup $MASTER_USER_NAME >> /tmp/azuredeploy.log.torque.$$ 2>&1
-echo "DONE"
